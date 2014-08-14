f4e2237ee1b721b987d98e865cc7229aed94d50a
skr <shankar.krishna@hispark.in>
2014-08-14 14:29:15 +0530

    commit

---
+++ fibo/main.m
@@ -0,0 +1,30 @@
+//
+//  main.m
+//  fibo
+//
+//  Created by BSA Univ7 on 14/08/14.
+//  Copyright (c) 2014 tutorial. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        int a=2,b=3,c=0;
+        printf("swapping\n");
+    
+                    c=a;
+                    a=b;
+                    b=a;
+            printf(" a value is %d \n b value is %d\n",a,b);
+
+            }
+        
+        // insert code here...
+    //    NSLog(@"Hello, World!");
+    
+    return 0;
+}
+
